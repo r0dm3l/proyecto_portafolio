5cfeabbf8314201170aa8b717e35c57a39271061
r0dm3l <rod12151@gmail.com>
2022-12-07 20:55:56 -0500

    configuracion del settings.py y creacion del modelo proyecto

--- django_portafolio/portafolio/models.py
+++ django_portafolio/portafolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.db.models.fields import CharField,URLField
+from django.db.models.fields.files import ImageField
 # Create your models here.
+
+class proyectos(models.Model):
+    titulo = CharField(max_length=100)
+    descripcion = CharField(max_length=300)
+    imagen = ImageField(upload_to='statics/images/')
+    url = URLField(blank=True)
